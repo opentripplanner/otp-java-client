11bb12a475e0b5b36508d3142a494b6d9c7f7e2f
Leonard Ehrenfried <mail@leonard.io>
2023-08-10 11:45:52 +0200

    Switch to Apache client

--- src/main/java/org/opentripplanner/client/OtpApiClient.java
+++ src/main/java/org/opentripplanner/client/OtpApiClient.java
@@ -6,14 +6,16 @@ import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.opentripplanner.client.model.Coordinate;
 import org.opentripplanner.client.model.Pattern;
 import org.opentripplanner.client.model.RequestMode;
@@ -30,7 +32,7 @@ public class OtpApiClient {
   private static final Logger LOG = LoggerFactory.getLogger(OtpApiClient.class);
   private static final String DEFAULT_GRAPHQL_PATH = "/otp/routers/default/index/graphql";
 
-  private final HttpClient httpClient = HttpClient.newHttpClient();
+  private final CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
   private final URI graphQlUri;
   private final ObjectMapper mapper;
@@ -88,20 +90,17 @@ public class OtpApiClient {
 
   private JsonNode sendRequest(String formattedQuery) throws IOException, InterruptedException {
     LOG.debug("Sending GraphQL query to {}: {}", graphQlUri, formattedQuery);
+
     var body = mapper.createObjectNode();
     body.put("query", formattedQuery);
 
     var bodyString = mapper.writeValueAsString(body);
 
-    HttpRequest request =
-        HttpRequest.newBuilder()
-            .uri(graphQlUri)
-            .header("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString(bodyString))
-            .build();
-    HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
-    var jsonNode = mapper.readTree(response.body());
+    HttpPost httpPost = new HttpPost(graphQlUri);
+    var stringEntity = new StringEntity(bodyString, ContentType.APPLICATION_JSON);
+    httpPost.setEntity(stringEntity);
+    var response = httpClient.execute(httpPost);
+    var jsonNode = mapper.readTree(response.getEntity().getContent());
 
     LOG.debug("Received the following JSON: {}", jsonNode.toPrettyString());
     return jsonNode;
