b088f61075451e7d9447b519e9f5c459b2570890
Leonard Ehrenfried <mail@leonard.io>
2024-02-15 18:42:00 +0100

    Fix formatting

--- src/main/java/org/opentripplanner/client/model/Stop.java
+++ src/main/java/org/opentripplanner/client/model/Stop.java
@@ -2,5 +2,4 @@ package org.opentripplanner.client.model;
 
 import java.util.Optional;
 
-public record Stop(String name, String gtfsId, Optional<String> code) {
-}
+public record Stop(String name, String gtfsId, Optional<String> code) {}
