967201eecc147b28552b0a675dd5b0657abd4025
Tobias Walter <t.walter@raumobil.com>
2024-05-28 20:29:13 +0200

    feat(plan query): add leg properties to response
    add intermediate stops, rentedBikes boolean, headSign

---
+++ src/main/java/org/opentripplanner/client/model/IntermediatePlace.java
@@ -0,0 +1,6 @@
+package org.opentripplanner.client.model;
+
+import java.time.OffsetDateTime;
+
+public record IntermediatePlace(
+    String name, OffsetDateTime departureTime, OffsetDateTime arrivalTime, Stop stop) {}
--- src/main/java/org/opentripplanner/client/model/Leg.java
+++ src/main/java/org/opentripplanner/client/model/Leg.java
@@ -1,8 +1,10 @@
 package org.opentripplanner.client.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.OptionalDouble;
 
 public record Leg(
@@ -13,13 +15,15 @@ public record Leg(
     LegMode mode,
     Duration duration,
     double distance,
+    Optional<String> headsign,
     Route route,
     Trip trip,
     List<FareProductUse> fareProducts,
     OptionalDouble accessibilityScore,
     Agency agency,
-    LegGeometry legGeometry
-    ) {
+    LegGeometry legGeometry,
+    @JsonProperty("rentedBike") boolean rentedVehicle,
+    Optional<List<IntermediatePlace>> intermediatePlaces) {
 
   /** Is this leg using public transport? */
   public boolean isTransit() {
